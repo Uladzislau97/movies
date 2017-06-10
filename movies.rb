90950b0d8f392699868d8d8b9c2e0caad452233d
Uladzislau97 <VSvasilyev97@gmail.com>
2017-06-10 23:20:25 +0300

    add movies reading from file

--- movies.rb
+++ movies.rb
@@ -1,14 +1,20 @@
-good_movies = ['Matrix']
-bad_movies = ['Titanic']
+BASE_RATING = 8.0
 
 if ARGV.any?
-  movie = ARGV[0]
+  file_name = ARGV[0]
+else
+  file_name = 'movies.txt'
+  puts "Warning! File name wasn't passed. Movies.txt is used by default.\n\n"
+end
 
-  if good_movies.include?(movie)
-    puts "#{movie} is a good movie"
-  elsif bad_movies.include?(movie)
-    puts "#{movie} is a bad movie"
-  else
-    puts "Haven't seen #{movie} yet"
-  end
+File.open(file_name, 'r').each do |line|
+  movie_data = line.split('|')
+  name = movie_data[1]
+  rating = movie_data[7].to_f
+
+  next unless name.include?('Max')
+
+  stars_amount = (rating * 10 - BASE_RATING * 10).to_i
+  rating_stars = '*' * stars_amount
+  puts "#{name} #{rating_stars}"
 end
